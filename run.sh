05755b3d656d56e250e1d62a4aa3439d62b21588
Kevin Bell <kevin.a.bell@gmail.com>
2023-08-16 23:21:21 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+curl --request POST \
+  --url "https://api.github.com/repos/$TEST_REPO/issues" \
+  --header "authorization: Bearer $GITHUB_TOKEN" \
+  --header 'content-type: application/json' \
+  --data '{
+    "title": "Automated issue from test-action",
+    "body": "This issue was automatically created by the GitHub Action workflow."
+    }' \
+  --fail
